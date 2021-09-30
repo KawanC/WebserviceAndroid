79947271d67eecc7abc2456ac3d5e2ad7fe83f0f
Thalisson <occtober456@outlook.com>
2021-09-29 21:35:22 -0300

    aa

--- app/src/main/java/com/tcc/webserviceandroid/MainActivity.java
+++ app/src/main/java/com/tcc/webserviceandroid/MainActivity.java
@@ -3,13 +3,19 @@ package com.tcc.webserviceandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,7 +41,28 @@ public class MainActivity extends AppCompatActivity {
                         Request.Method.GET, //Método que será usado
                         url, //Caminho da API do webservice
                         null, //Não haverá parâmetros a serem passados
-                        new
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                //Percorrer cada posicao do Array
+                                for(int i=0 ; i< response.length() ; i++){
+                                    try {
+                                        //Recuperar cada objeto do vetor
+                                        JSONObject obj = response.getJSONObject(i);
+                                        //exibir os dados no LogCat
+                                        Log.d("TESTEWS", "ID: " + obj.getString("id"));
+                                        Log.d("TESTEWS", "Nome: " + obj.getString("nome"));
+                                        Log.d("TESTEWS", "Preço: " + obj.getString("preco"));
+                                        Log.d("TESTEWS", "Categoria: " + obj.getString("categoria"));
+                                        Log.d("TESTEWS", "----------------------------------------");
+                                    }catch (JSONException e) {
+                                        e.printStackTrace();
+
+                                }
+                            }
+                        }
+
+
                 )
             }
         });
